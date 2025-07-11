9671bd3b57f3b45f5814dc488c645b4c7b25c7ef
stefanocasafranca <scasafrancal01@gmail.com>
2025-07-11 16:34:11 -0500

    refactor: update Design Process component and MDX content
    - Refactored the Design Process component to use a dynamic URL based on the method name.
    - Updated the anchor text to "View Method" for clarity.
    - Revised the description in the MDX file to better reflect the purpose of the method card.
    - Consolidated image array formatting in the MDX file for consistency.

--- components/grid/widgets/design-process.tsx
+++ components/grid/widgets/design-process.tsx
@@ -2,8 +2,11 @@ import Card from '../../ui/card';
 import Image from 'next/image';
 import Anchor from '../../ui/anchor';
 import { FaArrowRight } from 'react-icons/fa6';
+import { toKebabCase } from '@/utils/lib';
 
 export default function DesignProcess() {
+    const methodName = 'Design Process';
+
     return (
         <Card className='group relative flex flex-col items-center justify-center gap-6 p-8'>
             <div className='flex flex-1 items-center justify-center w-full h-full'>
@@ -21,10 +24,10 @@ export default function DesignProcess() {
             <div className='absolute bottom-3 left-3'>
                 <Anchor
                     className='cancel-drag size-10 justify-end transition-all ease-in-out group-hover:w-full'
-                    href='/methods/design-process'
-                    aria-label='Design Process'>
+                    href={`/methods/${toKebabCase(methodName)}`}
+                    aria-label={methodName}>
                     <span className='hidden whitespace-nowrap opacity-0 transition-all duration-300 ease-in group-hover:translate-x-0 group-hover:opacity-100 md:inline'>
-                        Design Process
+                        View Method
                     </span>
                     <span>
                         <FaArrowRight className='-rotate-45 transition-transform duration-300 group-hover:rotate-0' />
